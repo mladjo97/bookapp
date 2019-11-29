c98e9f4f9dc8f508cd807d3bc0ba502a49d2d50f
mladjo97 <mldnmilosevic@gmail.com>
2019-11-29 16:30:33 +0100

    Add side menu icon

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import Icon from '@expo/vector-icons/Ionicons'; 
+
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
@@ -10,15 +13,40 @@ import Feed from './FeedScreen';
 import Settings from './SettingsScreen';
 
 // Dashboard Tab Navigation
-const DashboardTabNavigator = createBottomTabNavigator({
-  Feed,
-  Profile,
-  Settings
-});
+const DashboardTabNavigator = createBottomTabNavigator(
+  {
+    Feed,
+    Profile,
+    Settings,
+  },
+  {
+    navigationOptions: ({ navigation }) => {
+      const { routeName } = navigation.state.routes[navigation.state.index];
+      return {
+        headerTitle: routeName
+      }
+    }
+  }
+);
 
-const DashboardStackNavigator = createStackNavigator({
-  DashboardTabNavigator: DashboardTabNavigator
-});
+const DashboardStackNavigator = createStackNavigator(
+  {
+    DashboardTabNavigator: DashboardTabNavigator
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => {
+      return {
+        headerLeft: (
+        <Icon 
+          style={{ paddingLeft: 10 }}
+          onPress={() => navigation.openDrawer()} 
+          name="md-menu" size={30}
+        />)
+      }
+    }
+  }
+);
+
 // Drawer Navigation
 const AppDrawerNavigator = createDrawerNavigator({
   Dashboard: {
