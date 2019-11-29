4ef8b5ed9405de60ed71b6839484191f161c66b0
mladjo97 <mldnmilosevic@gmail.com>
2019-11-29 14:49:35 +0100

    Add drawer navigation
    Added side draw navigator

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,12 +1,24 @@
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+
 
 import WelcomeScreen from './WelcomeScreen';
 import DashboardScreen from './DashboardScreen';
 
-// Screen routing
-const AppSwitchNavigator = createSwitchNavigator({
+// Drawer Navigation
+const AppDrawerNavigator = createDrawerNavigator({
+  Dashboard: {
+    screen: DashboardScreen
+  },
   Welcome: {
     screen: WelcomeScreen
+  }
+});
+
+// Screen Navigation
+const AppSwitchNavigator = createSwitchNavigator({
+  Welcome: {
+    screen: AppDrawerNavigator
   },
   Dashboard: {
     screen: DashboardScreen
